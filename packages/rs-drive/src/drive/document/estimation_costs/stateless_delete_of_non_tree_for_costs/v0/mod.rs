85a06dc02f5101bb3e48d403bdb85924cc39498f
pauldelucia <pauldelucia2@gmail.com>
2023-08-29 16:00:12 +0800

    feat: add docs

--- packages/rs-drive/src/drive/document/estimation_costs/stateless_delete_of_non_tree_for_costs/v0/mod.rs
+++ packages/rs-drive/src/drive/document/estimation_costs/stateless_delete_of_non_tree_for_costs/v0/mod.rs
@@ -14,29 +14,48 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 impl Drive {
-    /// Attempts a stateless deletion of non-tree elements for costs estimation.
+    /// Deletes an element in a stateless manner for computing costs.
     ///
-    /// This function either executes a stateful batch delete or a stateless batch delete based
-    /// on the presence of the `estimated_costs_only_with_layer_info` parameter.
-    ///
-    /// - When `estimated_costs_only_with_layer_info` is `None`, it directly performs a stateful batch delete.
-    /// - When `estimated_costs_only_with_layer_info` is `Some`, it retrieves the relevant layer
-    ///   information and performs a stateless batch delete. In this case, any missing layer
-    ///   information results in an error.
+    /// This function performs a stateless delete operation, specifically for 
+    /// computing costs in GroveDB.
     ///
     /// # Parameters
-    /// - `element_estimated_sizes`: An estimate of the layer sizes for the element to be deleted.
-    /// - `key_info_path`: The path of the key for which the deletion is to be estimated.
-    /// - `is_known_to_be_subtree_with_sum`: Optional information about the subtree and sum-subtree status.
-    /// - `estimated_costs_only_with_layer_info`: Optionally, a reference to the estimated costs with layer info.
+    ///
+    /// - `element_estimated_sizes`: Estimated sizes of the layer to be deleted.
+    /// - `key_info_path`: Path information to locate the element in the tree.
+    /// - `is_known_to_be_subtree_with_sum`: Specifies whether the node is known to be a subtree 
+    ///   and whether it's a sum-subtree. This is an `Option` containing a tuple where
+    ///   - `IsSubTree` signifies whether it's a subtree or not.
+    ///   - `IsSumSubTree` signifies whether it's a sum-subtree or not.
+    /// - `estimated_costs_only_with_layer_info`: Mutable reference to an optional hashmap that holds 
+    ///   layer information for cost estimation.
     ///
     /// # Returns
-    /// - `Ok(BatchDeleteUpTreeApplyType)`: The type of batch delete operation (either stateful or stateless).
-    /// - `Err(Error)`: An error if there is a problem retrieving layer information.
+    ///
+    /// - `Ok(BatchDeleteUpTreeApplyType)`: The type of batch delete that should be applied up the tree.
+    /// - `Err(Error)`: An error occurred, which is defined by the custom `Error` type.
     ///
     /// # Errors
-    /// Returns an `Error::Fee(FeeError::CorruptedEstimatedLayerInfoMissing)` if the required layer
-    /// information is missing in the provided estimated costs.
+    ///
+    /// - `Error::Fee(FeeError::CorruptedEstimatedLayerInfoMissing)`: Indicates missing layer information
+    ///   in `estimated_costs_only_with_layer_info`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// // Assuming all types and Error variants are appropriately defined
+    /// let estimated_sizes = EstimatedLayerSizes { /* fields */ };
+    /// let key_info_path = &KeyInfoPath::new(/* arguments */);
+    /// let subtree_info = Some((IsSubTree::Yes, IsSumSubTree::No));
+    /// let mut layer_info: Option<HashMap<KeyInfoPath, EstimatedLayerInformation>> = Some(HashMap::new());
+    ///
+    /// let result = Drive::stateless_delete_of_non_tree_for_costs_v0(
+    ///     estimated_sizes,
+    ///     key_info_path,
+    ///     subtree_info,
+    ///     &mut layer_info
+    /// );
+    /// ```
     pub(super) fn stateless_delete_of_non_tree_for_costs_v0(
         element_estimated_sizes: EstimatedLayerSizes,
         key_info_path: &KeyInfoPath,
